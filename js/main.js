0e19200e5b82d606c7557aff286199db4fffafb7
francesca-giorno <francescagiorno1996@gmail.com>
2020-11-16 14:01:44 +0100

    complete ex

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Aggiungere un’immagine presa anch’essa da un data.
+*/
+
+
+var container = new Vue({
+  el: "#container",
+  data:{
+    messaggio: "Benvenuto in Vue",
+    img:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/555px-Vue.js_Logo_2.svg.png"
+
+  }
+})
